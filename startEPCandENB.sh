baa9a89df5afefab24d769d9f3f1c12ca1716999
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2021-07-21 17:42:48 -0500

    Add files via upload
    added screen

--- startEPCandENB.sh
+++ startEPCandENB.sh
@@ -7,14 +7,14 @@ EXEC_PATH=/home/openairinterface5g/cmake_targets/lte_build_oai/build/lte-softmod
 CONFIG_PATH=/home/openairinterface5g/ci-scripts/conf_files/enb.band7.tm1.100PRB.usrpb210.conf
 
 if [ $MODE == "TESTBED" ]
-then 
+then
   mkdir /dev/net
   mknod /dev/net/tun c 10 200
   ip tuntap add mode tun srs_spgw_sgi
   ifconfig srs_spgw_sgi 172.16.0.1/24
-  srsepc &
-  $EXEC_PATH -O $CONFIG_PATH |  ts '[%Y-%m-%d %H:%M:%sS]' > OAI_ENB.log
-  #gnome-terminal -x bash -c "$EXEC_PATH -O $CONFIG_PATH | ts '[%Y-%m-%d %H:%M:%.S]' > OAI_ENB.log;exec bash" &
+screen -S EPC -dm bash -c "srsepc | ts '[%Y-%m-%d %H:%M:%.S]' > srsEPC.log"
+
+screen -S eNB -dm bash -c "$EXEC_PATH -O $CONFIG_PATH |  ts '[%Y-%m-%d %H:%M:%sS]' > OAI_ENB.log"
 #elif [ $MODE == "IQ_EMULATION" ]
 #then
 #  mkdir /dev/net
@@ -22,7 +22,7 @@ then
 #  ip tuntap add mode tun srs_spgw_sgi
 #  ifconfig srs_spgw_sgi 172.16.0.1/24
 #  srsepc &
-#  ./$EXEC_PATH -O $CONFIG_PATH & 
+#  ./$EXEC_PATH -O $CONFIG_PATH &
 #elif [ $MODE == "IQ_DIRECT" ]
 #then
 #  mkdir /dev/net
@@ -30,13 +30,20 @@ then
 #  ip tuntap add mode tun srs_spgw_sgi
 #  ifconfig srs_spgw_sgi 172.16.0.1/24
 #  srsepc &
-#  ./$EXEC_PATH -O ~/$CONFIG_PATH  & 
+#  ./$EXEC_PATH -O ~/$CONFIG_PATH  &
 elif [ $MODE == "PL_EMULATION" ]
 then
-  python3 zmq_tun.py -c -i ${CHEM_IP_ADDR} -p 5001 -t 172.16.0.1 -n tun &
+
+  screen -S zmqtun -dm bash -c "python3 zmq_tun.py -c -i ${CHEM_IP_ADDR} -p 5001 -t 172.16.0.1 -n tun "
+ sleep 60
+ screen -S ping -dm bash -c "ping -w 60 192.168.200.2 | ts '[%Y-%m-%d %H:%M:%.S]' > ping.log"
+
 elif [ $MODE == "PL_DIRECT" ]
 then
-  python3 zmq_tun.py -s -p 5002 -t 172.16.0.1 -n tun &
+  screen -S zmqtun -dm bach -c "python3 zmq_tun.py -s -p 5002 -t 172.16.0.1 -n tun"
+
 else
   echo "No mode specified"
+
+
 fi
